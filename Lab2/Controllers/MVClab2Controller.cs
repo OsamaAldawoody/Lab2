8c028d7d21ab08d76fa1f3e3875c48cc0c2547ac
OsamaAldawoody <frouzen1650@gmail.com>
2020-02-23 10:43:23 +0200

    Initial commit

---
+++ Lab2/Controllers/MVClab2Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab2.Models;
+
+
+namespace Lab2.Controllers
+{
+    public class MVClab2Controller : Controller
+    {
+        // GET: MVClab2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+
+
+            List<Courses> courses = new List<Courses>()
+            {
+                 new Courses() { id = 1, name = "c sharp" },
+                 new Courses() { id = 2, name = "java" },
+                new Courses() { id = 3, name = "php" },
+                 new Courses() { id = 4, name = "js" }
+        };
+
+            return View(courses);
+         
+        }
+    }
+}
---
+++ Lab2/Models/Courses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Courses
+    {
+        public int id{ get; set; }
+        public string name  { get; set; }
+    }
+}
